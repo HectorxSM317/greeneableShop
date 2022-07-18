4645c456760952426cef6888b98b16371afb5919
Maximiliano Palavecino <maxipp042@gmail.com>
2022-07-18 10:16:32 -0300

    added card component

---
+++ frontend/src/components/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Card() {
+    return (
+        <div>
+            <h2>Producto</h2>
+            <p>Texto informativo</p>
+            <p>Precio</p>
+        </div>
+    )
+}
