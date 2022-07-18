c171329496f11dc2140516544bbde35071d7dc23
Maximiliano Palavecino <maxipp042@gmail.com>
2022-07-18 10:08:09 -0300

    edit carousel

--- frontend/src/components/carousel.jsx
+++ frontend/src/components/carousel.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function carousel() {
     return (
+        <>
         <div>Soy un carousel</div>
+        <h2>Holaaaa soy un titulo</h2>
+        </>
     )
 }
