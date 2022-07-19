c516381f84938f5b5d4a87c8b399f8862662346c
NataliaBarrionuevo <nandreabarrionuevo@gmail.com>
2022-07-19 19:52:57 -0300

    description

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,7 +42,7 @@ function App() {
 
         <Route path="/" element={<Index />} />
         {/* <Route path="/products" element={<Products />} /> */}
-        {/* <Route path="/details" element={<Details />} /> */}
+        <Route path="/details" element={<Details />} />
         {/* {<Route path="/signUp" element={!loggedUser ? <SignUp /> : <Index />} />}
         {<Route path="/signIn" element={!loggedUser ? <SignIn /> : <Index />} />} */}
         
