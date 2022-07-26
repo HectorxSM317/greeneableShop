87ebcca831ebdcce78ca059c22b1b603711430a8
unknown <mguadalupecaruso@gmail.com>
2022-07-25 21:35:31 -0300

    register

--- config/validator.js
+++ config/validator.js
@@ -44,7 +44,7 @@ const validator = (req, res, next) => {
     city: joi.string(),
     address: joi.string(),
 
-    // photo: joi.string().required(),
+    photo: joi.string(),
    
     phoneNumber: joi.string(),
   });
